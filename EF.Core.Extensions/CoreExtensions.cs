a359e9cf9e2e60c2d40b84492efcc3321837cf95
Hassnain Ali <ess.hassnain@outlook.com>
2022-12-19 11:51:41 +0500

    added where if func overoad

--- EF.Core.Extensions/CoreExtensions.cs
+++ EF.Core.Extensions/CoreExtensions.cs
@@ -3,6 +3,24 @@
 public static class QueryableExtensions
 {
     #region Where
+    public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, bool condition, Func<T, bool> expression)
+        => condition ? source.Where(expression) : source;
+
+    public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, bool condition, Func<T, int, bool> expression)
+        => condition ? source.Where(expression) : source;
+
+    public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, Func<bool> condition, Func<T, bool> expression)
+        => condition() ? source.Where(expression) : source;
+
+    public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, Func<bool> condition, Func<T, int, bool> expression)
+        => condition() ? source.Where(expression) : source;
+
+    public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, Expression<Func<bool>> condition, Func<T, bool> expression)
+        => condition.Compile()() ? source.Where(expression) : source;
+
+    public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, Expression<Func<bool>> condition, Func<T, int, bool> expression)
+        => condition.Compile()() ? source.Where(expression) : source;
+
     public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, bool condition, Expression<Func<T, bool>> expression)
         => condition ? source.Where(expression) : source;
 
